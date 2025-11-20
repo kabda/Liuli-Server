d118ceb982b3b5ac109374be080fd1b35e3e3c2d
Kabda <xmacn@qq.com>
2025-11-20 23:29:18 +0800

    init

---
+++ Liuli-ServerTests/Liuli_ServerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Liuli_ServerTests.swift
+//  Liuli-ServerTests
+//
+//  Created by 樊远东 on 2025/11/20.
+//
+
+import Testing
+@testable import Liuli_Server
+
+struct Liuli_ServerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
